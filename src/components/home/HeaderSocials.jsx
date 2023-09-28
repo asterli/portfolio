badabe9584dfb78c674f195945a65fffa8c1229f
aster <asli@scu.edu>
2023-09-28 16:07:27 -0700

    Create sidebar and replace text with icons

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="https://www.linkedin.com/in/asterli12/" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+
+        <a href="https://github.com/asterli" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/asterli12/" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/asterli" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-square-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from './HeaderSocials';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Aster Li</h1>
+        <span className="home__education">I'm a junior studying Computer Science & Engineering.</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Get to know me!</a>
+      </div>
+    </section>
   )
 }
 
