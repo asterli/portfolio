ce41b40f004bedd6caa893a5d92e5ecb6b2f7abe
aster <asli@scu.edu>
2023-09-28 17:30:01 -0700

    Write and style social icons & scroll down button

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -10,14 +10,6 @@ const HeaderSocials = () => {
         <a href="https://github.com/asterli" className="home__social-link" target="_blank">
         <i class="fa-brands fa-github"></i>
         </a>
-
-        <a href="https://www.linkedin.com/in/asterli12/" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-linkedin"></i>
-        </a>
-
-        <a href="https://github.com/asterli" className="home__social-link" target="_blank">
-        <i class="fa-brands fa-square-github"></i>
-        </a>
     </div>
   )
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./home.css";
 import Me from "../../assets/avatar-1.svg";
 import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
         <HeaderSocials />
 
         <a href="#contact" className="btn">Get to know me!</a>
+
+        <ScrollDown />
       </div>
     </section>
   )
